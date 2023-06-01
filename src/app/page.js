bb06891835274a0f00c4da63c67d6e757087bdcc
David Castro <davidcastrosi@hotmail.com>
2023-06-01 16:40:54 -0600

    commit 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,9 @@
-// import { Accordion } from '../../components/ui/accordion'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '@/components/ui/accordion'
 
 export default function Home () {
   return (
@@ -297,6 +302,17 @@ export default function Home () {
             </div>
           </div>
         </div>
+      </section>
+      <section class=''>
+        <h1>HOla</h1>
+        <Accordion type='single' collapsible>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </section>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
         primary: '#c99836',
         background: '#2ead6c'
 
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       }
     }
   },
