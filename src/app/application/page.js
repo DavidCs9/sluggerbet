5a1d07a8c65e8b0a17bf24c28ae158d5ebc49a5c
David Castro <davidcastrosi@hotmail.com>
2023-06-02 18:58:58 -0600

    fix bg bugs

--- src/app/application/page.js
+++ src/app/application/page.js
@@ -5,8 +5,8 @@ import animationData from 'public/construction.json'
 
 export default function Page () {
   return (
-    <div className='m-20 mt-52'>
-      <h1 className='text-center text-4xl lg:text-5xl font-extrabold mb-20'>
+    <div className=' bg-gray-950 h-screen p-20'>
+      <h1 className='text-center text-4xl lg:text-5xl font-extrabold pb-20'>
         La aplicacion esta en construccion...
       </h1>
       <div className=' lg:w-2/6 mx-auto'>
